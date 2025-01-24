c75c6a0cf1acb3c0fcc7b60622125df6550a6c8f
Sandeep Singh <sandeepsinghpreet80055@gmail.com>
2025-01-24 08:31:35 +0530

    http module added

--- Introduction/3.httpModule.js
+++ Introduction/3.httpModule.js
@@ -2,6 +2,8 @@ const http = require("node:http");
 const port = 8000;
 // Create a local server to receive data from
 const server = http.createServer((req, res) => {
+  // request :- client data
+  // response :- server data
   res.writeHead(200, { "Content-Type": "application/json" });
   res.end(
     JSON.stringify({
