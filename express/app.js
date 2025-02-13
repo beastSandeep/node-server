9abadedb73d136d7b63c6b34685de710f4dd97dc
Sandeep Singh <sandeepsinghpreet80055@gmail.com>
2025-02-13 08:31:31 +0530

    startic added

--- express/app.js
+++ express/app.js
@@ -2,13 +2,19 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
-app.set("view engine", "pug");
+app.set("view engine", "pug"); /// npm i pug
 app.set("views", path.resolve(__dirname, "./views"));
 
+//Serving static files
+app.use(express.static(path.join(__dirname, "/public")));
+
 app.get("/", function (req, res) {
-  res
-    .status(200)
-    .render("index", { t: "my dynamic title", m: "anything", value: 10 });
+  res.status(200).render("index");
+});
+
+app.post("/upload", (req, res) => {
+  console.log(req.body);
+  res.end("done");
 });
 
 app.listen(3000, () => {
