a8ae1059b97a57700198aed934c205846c4e332f
Sandeep Singh <sandeepsinghpreet80055@gmail.com>
2025-02-22 08:36:07 +0530

    reading a file via fs

--- express/index.js
+++ express/index.js
@@ -1,5 +1,8 @@
 const express = require("express");
+const fs = require("node:fs");
 const app = express();
+
+const textPath = "D:/Server/express/text.txt";
 
 // all routes
 app.get("/", (req, res) => {
@@ -9,6 +12,16 @@ app.get("/", (req, res) => {
 
 app.get("/ollie", (req, res) => {
   res.status(200).json({ name: "ollie", home: "USA", owner: "ghotra family" });
+});
+
+app.get("/readFile", (req, res) => {
+  // reading file and stroring data into a constant
+  const data = fs.readFileSync(textPath, {
+    encoding: "utf-8",
+  });
+
+  // sending response with data
+  res.status(200).json({ result: data });
 });
 
 app.listen(3000, () => {
